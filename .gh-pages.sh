b7e1cd8d459bc10de1c0e1d7f5ad9b51572d66f7
KaFai Choi <kafai@river.com>
2019-12-27 17:38:04 +0700

    remove junks

--- .gh-pages.sh
+++
@@ -1,9 +0,0 @@
-stack exec site clean
-stack exec site build
-git checkout -b master --track origin/master
-cp -a _site/. .
-git add -A
-git commit -m "Publish."
-git push origin master:master
-git checkout develop
-git branch -D master
